aa71ddcbffde8198b45c61a645f9916af22fb6e0
AGD <agdholo@gmail.com>
2018-06-14 09:08:29 +0800

    4.2 样式美化

---
+++ application/welcome/view/index/about.blade.php
@@ -0,0 +1,11 @@
+
+@extends('_layout.default') 
+@section('title', '关于') 
+@section('content')
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">关于页面</h1>
+    </div>
+</section>
+@stop
+                                        
---
+++ application/welcome/view/index/help.blade.php
@@ -0,0 +1,11 @@
+
+@extends('_layout.default') 
+@section('title', '帮助') 
+@section('content')
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">帮助页面</h1>
+    </div>
+</section>
+@stop
+                                        
