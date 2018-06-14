ef70b59c130a60930322cf5f5161627280373a19
AGD <agdholo@gmail.com>
2018-06-14 09:26:26 +0800

    4.6 集中视图

--- config/template.php
+++ config/template.php
@@ -17,9 +17,9 @@ return [
     // 模板引擎类型
     'type' => 'Blade',
     // 视图基础目录（集中式）
-    'view_base' => '',
+    'view_base' => Env::get('ROOT_PATH') . 'resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR,
     // 模板起始路径
-    'view_path' => '',
+    'view_path' => Env::get('ROOT_PATH') . 'resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR,
     // 模板文件名分隔符
     'view_depr' => DIRECTORY_SEPARATOR,
     // 模板文件后缀
