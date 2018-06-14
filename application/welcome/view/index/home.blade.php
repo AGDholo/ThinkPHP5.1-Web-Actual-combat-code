9a3382f626c32ef2d88dc675f36686b88c5fc267
AGD <agdholo@gmail.com>
2018-06-14 08:56:17 +0800

    4.2 样式美化

---
+++ application/welcome/view/index/home.blade.php
@@ -0,0 +1,9 @@
+@extends('_layout.default') 
+@section('title', '主页') 
+@section('content')
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">欢迎你，我的朋友</h1>
+    </div>
+</section>
+@stop
--- config/template.php
+++ config/template.php
@@ -14,22 +14,19 @@
 // +----------------------------------------------------------------------
 
 return [
-    // 模板引擎类型 支持 php think 支持扩展
-    'type'         => 'Think',
-    // 默认模板渲染规则 1 解析为小写+下划线 2 全部转换小写 3 保持操作方法
-    'auto_rule'    => 1,
-    // 模板路径
-    'view_path'    => '',
-    // 模板后缀
-    'view_suffix'  => 'html',
+    // 模板引擎类型
+    'type' => 'Blade',
+    // 视图基础目录（集中式）
+    'view_base' => '',
+    // 模板起始路径
+    'view_path' => '',
     // 模板文件名分隔符
-    'view_depr'    => DIRECTORY_SEPARATOR,
-    // 模板引擎普通标签开始标记
-    'tpl_begin'    => '{',
-    // 模板引擎普通标签结束标记
-    'tpl_end'      => '}',
-    // 标签库标签开始标记
-    'taglib_begin' => '{',
-    // 标签库标签结束标记
-    'taglib_end'   => '}',
+    'view_depr' => DIRECTORY_SEPARATOR,
+    // 模板文件后缀
+    'view_suffix' => 'blade.php',
+    'cache' => [
+        'cache_subdir' => false,
+        'prefix' => '',
+    ],
+    'tpl_replace_string' => [], 
 ];
