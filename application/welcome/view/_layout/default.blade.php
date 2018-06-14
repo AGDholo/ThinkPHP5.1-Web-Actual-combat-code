ca19f35a9b5eeb1d701e61c6a553973a61a36e5a
AGD <agdholo@gmail.com>
2018-06-14 09:08:38 +0800

    4.3 局部视图

--- application/welcome/view/_layout/default.blade.php
+++ application/welcome/view/_layout/default.blade.php
@@ -15,38 +15,11 @@
   </head>
 
   <body>
-    <header>
-      <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container">
-            <a class="navbar-brand" href="#">ThinkPHP 入门教程</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-              aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">主页
-                    <span class="sr-only">(current)</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">帮助</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">关于</a>
-                </li>
-              </ul>
-              <a class="btn btn-outline-success my-2 my-sm-0" href="#">注册</a>
-            </div>
-          </div>
-        </nav>
-      </div>
-    </header>
+    @include('_layout.header')
     <div class="container">
       @yield('content')
     </div>
+    @include('_layout.footer')
     <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
       crossorigin="anonymous"></script>
     <script src="https://cdn.bootcss.com/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
---
+++ application/welcome/view/_layout/header.blade.php
@@ -0,0 +1,29 @@
+<header>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">ThinkPHP 入门教程</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">主页
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/help">帮助</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">关于</a>
+            </li>
+          </ul>
+          <a class="btn btn-outline-success my-2 my-sm-0" href="#">注册</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</header>
