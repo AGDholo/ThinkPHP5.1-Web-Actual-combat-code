dac6573755dd299ebb4e4585cd7809a1abc46cac
AGD <agdholo@gmail.com>
2018-11-12 16:45:55 +0800

    6.2 注册表单

--- resources/views/user/auth/create.blade.php
+++ resources/views/user/auth/create.blade.php
@@ -1,11 +1,54 @@
-
-@extends('_layout.default') 
-@section('title', '注册') 
+@extends('_layout.default')
+@section('title', '注册')
 @section('content')
-<section class="jumbotron text-center">
-    <div class="container">
-        <h1 class="jumbotron-heading">注册</h1>
+<div class="col-md-offset-2 col-md-8">
+    <div class="panel panel-default mt-5">
+        <div class="panel-heading mb-3">
+            <h4>注册</h4>
+        </div>
+        <div class="panel-body">
+            <form method="POST">
+                {:token()}
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">用户名</span>
+                    </div>
+                    <input type="text"
+                           class="form-control"
+                           name="name">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">邮箱</span>
+                    </div>
+                    <input type="text"
+                           class="form-control"
+                           name="email">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">密码</span>
+                    </div>
+                    <input type="text"
+                           class="form-control"
+                           name="password">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">确认密码</span>
+                    </div>
+                    <input type="text"
+                           class="form-control"
+                           name="password_confirmation">
+                </div>
+
+                <button type="submit"
+                        class="btn btn-primary btn-block">注册</button>
+            </form>
+        </div>
     </div>
-</section>
+</div>
 @stop
-                                    
